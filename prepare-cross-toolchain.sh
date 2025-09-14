ac5a8382d47354e1121bf65c6f60b10bd1a772f2
starg2 <starg2@envs.net>
2025-09-14 22:09:57 +0900

    Don't copy libc++ module sources

--- prepare-cross-toolchain.sh
+++ prepare-cross-toolchain.sh
@@ -72,5 +72,5 @@ for arch in $ARCHS; do
 done
 
 # Copy the libc++ module sources
-rm -rf $DEST/share/libc++
-cp -a $SRC/share/libc++ $DEST/share
+#rm -rf $DEST/share/libc++
+#cp -a $SRC/share/libc++ $DEST/share
